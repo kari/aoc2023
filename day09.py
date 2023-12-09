5ef518cd67d9f6005e1403219a12d2eb6402bab9
Kari Silvennoinen <kari@kalifi.org>
2023-12-09 10:32:06 +0200

    day 9 part 1

--- day09.py
+++ day09.py
@@ -1,18 +1,18 @@
 """ Day 9 of AoC 2023 """
 import numpy as np
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     INPUT = """0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45""".splitlines()
     PART1 = 114
-    PART2 = None
+    PART2 = 2
 else:
     with open("inputs/day09.txt", "r", encoding="utf8") as f:
         INPUT = f.read().splitlines()
-    PART1 = None
+    PART1 = 2038472161
     PART2 = None
 
 history = list(map(lambda x: list(map(int, x.split())), INPUT))
@@ -20,10 +20,13 @@ next_values = []
 for h in history:
     last_digits = [h[-1]]
     diff = h.copy()
-    while sum(diff) != 0:
+    while any([x != 0 for x in diff]):
         diff = np.diff(diff)
         last_digits.append(diff[-1])
-    print(list(reversed(last_digits)))
-    print(np.cumsum(list(reversed(last_digits))))
+#    print(list(reversed(last_digits)))
+#    print(np.cumsum(list(reversed(last_digits))))
     next_values.append(np.cumsum(list(reversed(last_digits)))[-1])
+
 print(sum(next_values))
+assert sum(next_values) == PART1
+
